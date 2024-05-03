79c961be67614fc2c2dd020af1903a714e148b11
Maurício Augusto <78917687+mrc-augusto@users.noreply.github.com>
2024-05-03 13:53:27 -0300

    initial routes and pages

---
+++ src/components/Header/index.tsx
@@ -0,0 +1,13 @@
+import { HeaderContainer } from './styles'
+
+export function Header() {
+  return (
+    <HeaderContainer>
+      <span>logo</span>
+      <nav>
+        <a href="">Timer</a>
+        <a href="">History</a>
+      </nav>
+    </HeaderContainer>
+  )
+}
