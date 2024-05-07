eff6cdde8e0b0078a9482717b7dd7aec1843825d
Maurício Augusto <78917687+mrc-augusto@users.noreply.github.com>
2024-05-07 12:44:08 -0300

    styles of pages and routes finished

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -8,8 +8,11 @@ import {
   StartCountdownButton,
   TaskInput,
 } from './styles'
+import { useState } from 'react'
 
 export function Home() {
+  const [task, setTask] = useState('')
+
   return (
     <HomeContainer>
       <form action="">
@@ -19,6 +22,8 @@ export function Home() {
             id="task"
             list="task-suggestions"
             placeholder="Dê um nome para o seu projeto"
+            onChange={(e) => setTask(e.target.value)}
+            value={task}
           />
 
           <datalist id="task-suggestions">
@@ -49,7 +54,7 @@ export function Home() {
           <span>0</span>
         </CountdownContainer>
 
-        <StartCountdownButton disabled type="submit">
+        <StartCountdownButton disabled={!task} type="submit">
           <Play size={24} />
           Começar
         </StartCountdownButton>
